a851099e52880c8261f1d32fd101aecafacbde83
lv10 <luisvillamarin@gmail.com>
2015-07-01 23:18:09 -0400

    re-arranged unit test, separated general functionality tests

--- tests/api_basics.py
+++ tests/api_basics.py
@@ -2,7 +2,6 @@ import unittest
 
 from nose.tools import raises
 
-from api.constants import BASE_URL
 from api.products import BestBuyAPI, BestBuyAPIError
 
 
@@ -14,21 +13,6 @@ class TestAPIBasics(unittest.TestCase):
 
     def tearDown(self):
         pass
-
-    def test_build_url(self):
-
-        sample_url = "{0}products(sku=43900)".format(BASE_URL)
-
-        payload = {
-            'query': "sku=43900",
-            'params': {'format': "json"}
-        }
-
-        url, thePayload = self.bestbuy._build_url(payload)
-
-        assert sample_url == url
-        assert (thePayload['format'] == "json"
-                and thePayload.get('apiKey') is not None)
 
     @raises(BestBuyAPIError)
     def test_validate_params(self):
